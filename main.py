8a724fcc6c360aace7e1d3abf513cbcb1c56d29a
Devraj Priyadarshi <devrajpriyadarshi01@gmail.com>
2022-07-22 23:34:35 +0530

    Added Rectification

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
 import numpy as np
+from numpy.linalg import inv
 import cv2 as cv
+from matplotlib import pyplot as plt
 # import open3d 
 
 img1 = cv.imread('Files/View1.png', 1)
@@ -31,6 +33,15 @@ R1 = np.array(R1)
 t1 = np.array(t1)
 R2 = np.array(R2)
 t2 = np.array(t2)
+P1 = K @ np.array(np.bmat([R1,t1]))
+P2 = K @ np.array(np.bmat([R2,t2]))
+
+p = np.array(np.bmat([[inv(K)],[np.zeros((1,3))]]))
+c = np.array([[0],[0],[0],[1]])
+# F = np.array(np.cross(P2, c)@P2@p)
+
+P2_ = P2.T @ inv(P2@P2.T)
+# F2 = np.array(np.cross(P1, _) @ P1 @ P2_)
 
 def scale(img, sx, sy):
     # print("Image is of ", img.shape)
@@ -50,9 +61,18 @@ def showCameraParameters():
     print("t1 =\n", t1,"\n")
     print("R2 =\n", R2,"\n")
     print("t2 =\n", t2,"\n")
+    print("P1 =\n", P1,"\n")
+    print("P2 =\n", P2,"\n")
+    print("p =\n", p,"\n")
+    print("c =\n", c,"\n")
+    # print("F =\n", F,"\n")
+
     print("")
 
 def segmentFace(img):
+
+    """Todo:REPLEACE THIS WITH COLOR-BASED SEGMENTATION"""
+
     mask = np.zeros(img.shape[:2],np.uint8)
     bgdModel = np.zeros((1,65),np.float64)
     fgdModel = np.zeros((1,65),np.float64)
@@ -81,6 +101,35 @@ def getSegmentedImgs2(img1=img1, img2=img2):
     
     return img1Seg, img2Seg
 
+def segmentSkin(img1, img2):
+    low_skin = np.array([69, 60, 90])
+    high_skin = np.array([195, 170, 255])
+    mask1 = cv.inRange(img1, low_skin, high_skin)
+    mask2 = cv.inRange(img2, low_skin, high_skin)
+    seg1 = cv.bitwise_and(img1, img1, mask = mask1)
+    seg2 = cv.bitwise_and(img2, img2, mask = mask2)
+    showImg(scale(seg1, 0.3, 0.3))
+    showImg(scale(seg2, 0.3, 0.3))
+
+def drawlines(img1src, img2src, lines, pts1src, pts2src):
+    ''' img1 - image on which we draw the epilines for the points in img2
+        lines - corresponding epilines '''
+    r, c, h = img1src.shape
+    # img1color = cv.cvtColor(img1src, cv.COLOR_GRAY2BGR)
+    # img2color = cv.cvtColor(img2src, cv.COLOR_GRAY2BGR)
+    img1color = img1src
+    img2color = img2src
+    # Edit: use the same random seed so that two images are comparable!
+    np.random.seed(0)
+    for r, pt1, pt2 in zip(lines, pts1src, pts2src):
+        color = tuple(np.random.randint(0, 255, 3).tolist())
+        x0, y0 = map(int, [0, -r[2]/r[1]])
+        x1, y1 = map(int, [c, -(r[2]+r[0]*c)/r[1]])
+        img1color = cv.line(img1color, (x0, y0), (x1, y1), color, 1)
+        img1color = cv.circle(img1color, tuple(pt1), 5, color, -1)
+        img2color = cv.circle(img2color, tuple(pt2), 5, color, -1)
+    return img1color, img2color
+
 def getRecktified(img1, img2):
     sift = cv.SIFT_create()
     # find the keypoints and descriptors with SIFT
@@ -102,7 +151,7 @@ def getRecktified(img1, img2):
     pts2 = []
 
     for i, (m, n) in enumerate(matches):
-        if m.distance < 0.4*n.distance:
+        if m.distance < 0.75*n.distance:
             matchesMask[i] = [1, 0]
             good.append(m)
             pts2.append(kp2[m.trainIdx].pt)
@@ -114,22 +163,60 @@ def getRecktified(img1, img2):
                         flags=cv.DrawMatchesFlags_DEFAULT)
 
     keypoint_matches = cv.drawMatchesKnn( img1, kp1, img2, kp2, matches[:], None, **draw_params)
-    showImg(scale(keypoint_matches, 0.2, 0.2))
+    # showImg(scale(keypoint_matches, 0.2, 0.2))
+
+    pts1 = np.int32(pts1)
+    pts2 = np.int32(pts2)
+    fundamental_matrix, inliers = cv.findFundamentalMat(pts1, pts2, cv.FM_RANSAC)
+
+    print("funda =\n",fundamental_matrix,"\n")
+    pts1 = pts1[inliers.ravel() == 1]
+    pts2 = pts2[inliers.ravel() == 1]
+
+    lines1 = cv.computeCorrespondEpilines(
+    pts2.reshape(-1, 1, 2), 2, fundamental_matrix)
+    lines1 = lines1.reshape(-1, 3)
+    img5, img6 = drawlines(img1, img2, lines1, pts1, pts2)
+
+    # Find epilines corresponding to points in left image (first image) and
+    # drawing its lines on right image
+    lines2 = cv.computeCorrespondEpilines(
+        pts1.reshape(-1, 1, 2), 1, fundamental_matrix)
+    lines2 = lines2.reshape(-1, 3)
+    img3, img4 = drawlines(img2, img1, lines2, pts2, pts1)
+
+    # plt.subplot(211), plt.imshow(img5)
+    # plt.subplot(212), plt.imshow(img3)
+    # plt.suptitle("Epilines in both images")
+    # plt.show()
+
+    h1, w1, c1 = img1.shape
+    h2, w2, c2 = img2.shape
+    _, H1, H2 = cv.stereoRectifyUncalibrated(
+        np.float32(pts1), np.float32(pts2), fundamental_matrix, imgSize=(w1, h1)
+    )
+    img1_rectified = cv.warpPerspective(img1, H1, (w1, h1))
+    img2_rectified = cv.warpPerspective(img2, H2, (w2, h2))
+    cv.imwrite("Files/rectified_1.png", img1_rectified)
+    cv.imwrite("Files/rectified_2.png", img2_rectified)
+    # showImg(scale(img1_rectified, 0.3, 0.3))
+    # showImg(scale(img2_rectified, 0.3, 0.3))
+    return img1_rectified, img2_rectified
+
     
 
 if __name__ == "__main__":
 
     logging.basicConfig(format='[%(asctime)s] %(levelname)s: %(message)s', level=logging.DEBUG)
 
-    # showImg(scale(img1, 0.1, 0.1))
+    # showImg(scale(img1, 0.3, 0.3))
     print("CAMERA PARAMETERS:\n")
     showCameraParameters()
 
+    """START RECTIFICATION"""
     logging.info("Starting Face Segementation from Background:")
-
     seg1 = cv.imread("Files/Segmented1.png", 1)
     seg2 = cv.imread("Files/Segmented2.png", 1)
-
     try:
         if seg1 == None or seg2 == None:
             logging.warning("No Saved Images Found. Will continue segmentation process.")
@@ -137,10 +224,11 @@ if __name__ == "__main__":
             seg1, seg2 = getSegmentedImgs()
     except ValueError:
         logging.info("FOUND SAVED SEGEMENTED IMAGES! Skipping Segementation")
-        
     logging.info("Segmentation Done!\n")
-    # showImg(seg1)
-    # showImg(scale(seg1, 0.3, 0.3))
+
+    """START RECTIFICATION"""
     logging.info("Starting Rectification!")
-    getRecktified(seg1, seg2)
+    rect1, rect2 = getRecktified(seg1, seg2)
     logging.info("Rectification Done!\n")
+
+    
